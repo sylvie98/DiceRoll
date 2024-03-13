9b45c161e814c0f63138a685cf9b306802f54de0
sylvie98 <sylviemasengesho5@gmail.com>
2024-03-13 09:30:03 +0200

    Dice Roll Simulator

---
+++ index.js
@@ -0,0 +1,46 @@
+const buttonElement =document.getElementById("roll-button");
+const diceElement =document.getElementById("dice");
+const rollHistoryElement=document.getElementById("roll-history");
+let historyList=[]
+function rollDice(){
+    const rollResult=Math.floor(Math.random() * 6) + 1;
+    const diceFace = getDiceFace(rollResult);
+    diceElement.innerHTML= diceFace 
+    historyList.push(rollResult)
+    updateRollHistory();
+}
+function updateRollHistory(){
+rollHistoryElement.innerHTML=""
+for(let i=0;i<historyList.length;i++){
+    const listItem=document.createElement("li");
+    listItem.innerHTML=`Roll${i+1}:<span>${getDiceFace(historyList[i])}</span>`
+    rollHistoryElement.appendChild(listItem);
+}
+}
+function getDiceFace(rollResult){
+    switch(rollResult){
+        case 1:
+            return "&#9856;";
+            case 2:
+                return "&#9857;";
+                case 3:
+                return "&#9858;";
+                case 4:
+                return "&#9859;";
+                case 5:
+                return "&#9860;";
+                case 6:
+                return "&#9861;";
+                default:
+                return "";
+                
+    }
+}
+buttonElement.addEventListener("click",() =>{
+    diceElement.classList.add("roll-animation");
+   setTimeout(()=>{
+    diceElement.classList.remove("roll-animation");
+    rollDice()  
+},1000);
+   
+});
